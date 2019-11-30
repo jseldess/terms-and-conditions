b96ab0e26fb8c98c64bda2978d8b63dd41db7f3d
Jesse Seldess <j_seldess@hotmail.com>
2019-11-30 14:10:30 -0500

    Scrape a terms/privacy webpage instead of using a text file

--- terms.py
+++ terms.py
@@ -1,22 +1,40 @@
+"""Create a new file with a random permutation of lines from a
+terms/privacy/data policy-type webpage and text file with poetry, with
+options to set a max number of lines, set a max number of words per line
+from the webpage, use only unique lines, and/or randomly skip lines.
+
+Help: python3 terms.py --help
+
+Example usage:
+
+Using the default webpage (Instagram data policy) and a poetry file,
+generate 10 lines, 8 words max per line from the webpage, with unique lines:
+    python3 terms.py -ml 10 -mw 8 -u
+Same as above, but using the Getty Images privacy policy webpage:
+    python3 terms.py -ml 10 -mw 8 -u -turl "https://www.gettyimages.com/company/privacy-policy"
+"""
+
 import argparse
+import html2text
 import os
 import re
 import random
+import requests
 from string import punctuation
 from time import gmtime, strftime
 
 parser = argparse.ArgumentParser(
-    description="""Create a new file with a random permutation of lines from two
-    source text files, one with terms and conditions and the other with poetry,
-    with options to set a max number of lines, set a max number of words per line
-    from the terms text, use only unique lines, and/or randomly skip lines.""")
-parser.add_argument("-tf", "--terms_file",
-                    help="""Source terms and conditions text file
-                    (default: source/terms_and_conditions.txt)""",
-                    default = "source/terms_and_conditions.txt")
+    description="""Create a new file with a random permutation of lines from a
+    terms/privacy/data policy-type webpage and text file with poetry, with
+    options to set a max number of lines, set a max number of words per line
+    from the webpage, use only unique lines, and/or randomly skip lines.""")
+parser.add_argument("-turl", "--terms_url",
+                    help="""URL of a terms/privacy/data policy-type webpage
+                    (default: Instagram's data policy)""",
+                    default="https://help.instagram.com/519522125107875")
 parser.add_argument("-pf", "--poetry_file",
                     help="Source poetry text file (default: source/source.txt)",
-                    default = "source/source.txt")
+                    default="source/source.txt")
 parser.add_argument("-d", "--new_file_dir",
                     help="""Path to the directory where the new file will be
                     created (default: generated_files)""",
@@ -32,7 +50,8 @@ parser.add_argument("-ml", "--max_lines", type=int,
                     help="Max number of lines of text in total")
 parser.add_argument("-mw", "--max_words_per_line", type=int,
                     help="""Max number of words per line, randomly retaining
-                    words from the start, middle, or end of the line""")
+                    words from the start, middle, or end of the line""",
+                    default=8)
 args = parser.parse_args()
 
 # Create a new directory for generated files, if it doesn't exist.
@@ -52,9 +71,14 @@ if args.max_words_per_line:
 filename += "_py.txt"
 new_file = open(os.path.join(args.new_file_dir, filename), "a", os.O_NONBLOCK)
 
+url = args.terms_url
+page = requests.get(url)
+text = html2text.html2text(page.text)
+# print(text)
+
 # Read source files and store their lines in terms and poetry.
-with open(args.terms_file) as terms, open(args.poetry_file) as poetry:
-    terms = terms.readlines()
+with open(args.poetry_file) as poetry:
+    terms = text.splitlines()
     poetry = poetry.readlines()
     lines_seen = set()
     total_lines = 0
@@ -63,6 +87,9 @@ with open(args.terms_file) as terms, open(args.poetry_file) as poetry:
         tline = random.choice(terms)
         terms.remove(tline)
         if not tline.isspace():
+            # If line is all uppercase, change to lowercase.
+            if tline.isupper():
+                tline = tline.lower()
             # If --max_lines is passed, break out of the loop
             # as soon as the max lines have been written.
             if args.max_lines:
@@ -78,7 +105,6 @@ with open(args.terms_file) as terms, open(args.poetry_file) as poetry:
                     middle = ' '.join(tline.split(' ')[remove // 2:-(remove // 2)])
                     end = ' '.join(tline.split(' ')[remove:])
                     tline = random.choice([start, middle, end])
-                    # print(tline)
             # If --random_skip is passed, randomly skip the line
             # and continue the next iteration of the loop.
             # To increase the chance of skips, add 1s to the list.
@@ -92,9 +118,12 @@ with open(args.terms_file) as terms, open(args.poetry_file) as poetry:
                 if tline.strip().lower() in lines_seen:
                     continue
                 lines_seen.add(tline.strip().lower())
-            if tline.isupper():
-                tline = tline.lower()
-            new_file.write(re.sub(r'[^\w\s]','', tline) + "\n")
+            # Make sure text is from one sentence, and remove punction
+            # and extra spaces.
+            tline = tline.split('.')[0]
+            tline = re.sub(r'[^\w\s]', ' ', tline).strip()
+            tline = re.sub(' +', ' ', tline)
+            new_file.write(tline + "\n")
             print("Write line: terms | Words removed: " + str(remove))
             total_lines += 1
             # Radomly write 0, 1, 2, 3, or 4 empty lines
