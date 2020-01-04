3fd8613fc69079fdd2b9f1974fd3e7e43bae743a
Jesse Seldess <j_seldess@hotmail.com>
2020-01-04 17:57:17 -0500

    Clean up

--- terms.py
+++ terms.py
@@ -43,9 +43,6 @@ parser.add_argument("-u", "--unique_lines",
                     action="store_true",
                     help="""Use each unique line from the source file only once
                     (default: False)""")
-parser.add_argument("-r", "--random_skip",
-                    action="store_true",
-                    help="Randomly skip writing lines (default: False)")
 parser.add_argument("-ml", "--max_lines", type=int,
                     help="Max number of lines of text in total")
 parser.add_argument("-mw", "--max_words_per_line", type=int,
@@ -62,8 +59,6 @@ if not os.path.exists(args.new_file_dir):
 filename = strftime("%Y-%m-%d-%H:%M:%S", gmtime())
 if args.unique_lines:
     filename += "_unique"
-if args.random_skip:
-    filename += "_skip"
 if args.max_lines:
     filename += "_maxlines" + str(args.max_lines)
 if args.max_words_per_line:
@@ -71,99 +66,95 @@ if args.max_words_per_line:
 filename += "_py.txt"
 new_file = open(os.path.join(args.new_file_dir, filename), "a", os.O_NONBLOCK)
 
-url = args.terms_url
-page = requests.get(url)
-text = html2text.html2text(page.text)
+
+# Set up the html-to-text maker.
+t_maker = html2text.HTML2Text()
+t_maker.ignore_links = True
+t_maker.ignore_images = True
+t_maker.ignore_emphasis = True
+
+# Get html from webpage and conver to plaintext.
+html = requests.get(args.terms_url)
+text = t_maker.handle(html.text)
 # print(text)
 
-# Read source files and store their lines in terms and poetry.
-with open(args.poetry_file) as poetry:
-    terms = text.splitlines()
-    poetry = poetry.readlines()
-    lines_seen = set()
-    total_lines = 0
-    while len(terms) > 0 and len(poetry) > 0:
-        # Randomly select a line from the terms text and remove it from source.
-        tline = random.choice(terms)
-        terms.remove(tline)
-        if not tline.isspace():
-            # If line is all uppercase, change to lowercase.
-            if tline.isupper():
-                tline = tline.lower()
-            # If --max_lines is passed, break out of the loop
-            # as soon as the max lines have been written.
-            if args.max_lines:
-                if total_lines >= args.max_lines:
-                    break
-            # If --max_words_from_line is passed, randomly select the specified
-            # num of words from the start, middle, or end of the line.
-            remove = 0
-            if args.max_words_per_line:
-                if len(re.findall(r'\w+', tline)) > args.max_words_per_line:
-                    remove = len(re.findall(r'\w+', tline)) - args.max_words_per_line
-                    start = ' '.join(tline.split(' ')[:-remove])
-                    middle = ' '.join(tline.split(' ')[remove // 2:-(remove // 2)])
-                    end = ' '.join(tline.split(' ')[remove:])
-                    tline = random.choice([start, middle, end])
-            # If --random_skip is passed, randomly skip the line
-            # and continue the next iteration of the loop.
-            # To increase the chance of skips, add 1s to the list.
-            if args.random_skip and random.choice([0, 1]) == 1:
-                print("Skip line: terms")
+# Read texts and store their lines in terms and poetry.
+terms = text.splitlines()
+poetry = open(args.poetry_file).readlines()
+lines_seen = set()
+total_lines = 0
+while len(terms) > 0 and len(poetry) > 0:
+    # If --max_lines is passed, break out of the loop
+    # as soon as the max lines have been written.
+    if args.max_lines:
+        if total_lines >= args.max_lines:
+            break
+    # Randomly select a line from the terms text and remove it from source.
+    tline = random.choice(terms)
+    terms.remove(tline)
+    if not tline.isspace() or len(tline) < 1:
+        # If line is all uppercase, change to lowercase.
+        if tline.isupper():
+            tline = tline.lower()
+        # Make sure text is from one sentence and remove punction.
+        tline = tline.split('.')[0]
+        print(tline)
+        tline = re.sub(r"[^\w\'\-\s]", "", tline).strip()
+        print(tline)
+        # If --max_words_from_line is passed, randomly select the specified
+        # num of words from the start, middle, or end of the line.
+        remove = 0
+        if args.max_words_per_line:
+            if len(re.findall(r'\w+', tline)) > args.max_words_per_line:
+                remove = len(re.findall(r'\w+', tline)) - args.max_words_per_line
+                start = ' '.join(tline.split(' ')[:-remove])
+                middle = ' '.join(tline.split(' ')[remove // 2:-(remove // 2)])
+                end = ' '.join(tline.split(' ')[remove:])
+                tline = random.choice([start, middle, end])
+        print(tline)
+        # If --unique_lines is passed, check if the line was
+        # seen in a previous iteration. If not, write the line
+        # to new_file and add it to lines_seen.
+        if args.unique_lines:
+            if tline.lower() in lines_seen:
                 continue
-            # If --unique_lines is passed, check if the line was
-            # seen in a previous iteration. If not, write the line
-            # to new_file and add it to lines_seen.
-            if args.unique_lines:
-                if tline.strip().lower() in lines_seen:
-                    continue
-                lines_seen.add(tline.strip().lower())
-            # Make sure text is from one sentence, and remove punction
-            # and extra spaces.
-            tline = tline.split('.')[0]
-            tline = re.sub(r'[^\w\s]', ' ', tline).strip()
-            tline = re.sub(' +', ' ', tline)
-            new_file.write(tline + "\n")
-            print("Write line: terms | Words removed: " + str(remove))
-            total_lines += 1
-            # Radomly write 0, 1, 2, 3, or 4 empty lines
-            # to new_file, with 0 weighted heavier.
-            new_file.write(random.choice([
-            "", "", "", "", "\n", "\n\n", "\n\n\n", "\n\n\n\n"]))
-            # new_file.write("\n\n")
-            print(args.max_lines, total_lines)
+            lines_seen.add(tline.lower())
+        if tline.isspace() or len(tline) < 1:
+            print("Skip empty line\n")
+            continue
+        if not tline.isascii():
+            print("Skip non-ascii line\n")
+            continue
+        new_file.write(tline + "\n")
+        print("Write line: terms | Words removed: ", str(remove))
+        total_lines += 1
+        print(total_lines, "/", args.max_lines, "\n")
+        # Radomly write 0, 1, 2, 3, or 4 empty lines
+        # to new_file, with 0 weighted heavier.
+        new_file.write(random.choice([
+        "", "", "", "", "\n", "\n\n", "\n\n\n", "\n\n\n\n"]))
 
-        # Randomly select a line from the poetry text and remove it from source.
-        pline = random.choice(poetry)
-        poetry.remove(pline)
-        if not pline.isspace():
-            # If --max_lines is passed, break out of the loop
-            # as soon as the max lines have been written.
-            if args.max_lines:
-                if total_lines >= args.max_lines:
-                    break
-            # If --random_skip is passed, randomly skip the line
-            # and continue the next iteration of the loop.
-            # To increase the chance of skips, add 1s to the list.
-            if args.random_skip and random.choice([0, 1]) == 1:
-                print("Skip line: poetry")
+    # Randomly select a line from the poetry text and remove it from source.
+    pline = random.choice(poetry)
+    poetry.remove(pline)
+    if not pline.isspace() or len(pline) < 1:
+        # Remove leading and trailing punction.
+        pline = pline.strip(punctuation)
+        # If --unique_lines is passed, check if the line was
+        # seen in a previous iteration. If not, write the line
+        # to new_file and add it to lines_seen.
+        if args.unique_lines:
+            if pline.lower() in lines_seen:
                 continue
-            # If --unique_lines is passed, check if the line was
-            # seen in a previous iteration. If not, write the line
-            # to new_file and add it to lines_seen.
-            if args.unique_lines:
-                if pline.strip().lower() in lines_seen:
-                    continue
-                lines_seen.add(pline.strip().lower())
-            new_file.write(pline.rstrip(punctuation))
-            print("Write line: poetry")
-            total_lines += 1
-            # Radomly write 0, 1, 2, 3, or 4 empty lines
-            # to new_file, with 0 weighted heavier.
-            new_file.write(random.choice([
-            "", "", "", "", "\n", "\n\n", "\n\n\n", "\n\n\n\n"]))
-            # new_file.write("\n\n")
-            print(args.max_lines, total_lines)
+            lines_seen.add(pline.lower())
+        new_file.write(pline)
+        print("Write line: poetry")
+        total_lines += 1
+        print(total_lines, "/", args.max_lines, "\n")
+        # Radomly write 0, 1, 2, 3, or 4 empty lines
+        # to new_file, with 0 weighted heavier.
+        new_file.write(random.choice([
+        "", "", "", "", "\n", "\n\n", "\n\n\n", "\n\n\n\n"]))
 
 
 print("File {} created".format(filename))
